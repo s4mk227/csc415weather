9da6aab2022b1421f1e3338b94773ddd71235b0f
holden <htwight@gmail.com>
2017-12-05 17:29:56 +0000

    Add necessary files to git

---
+++ WeatherApplication/app/src/main/java/androidclassproject/weatherapplication/LocationListActivity.java
@@ -0,0 +1,26 @@
+package androidclassproject.weatherapplication;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.app.FragmentManager;
+import android.app.ListFragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class LocationListActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.locationlist_activity);
+        FragmentManager fragmentManager = getFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.locationFragmentContainer);
+
+        if (fragment == null) {
+            fragment = new LocationListFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.locationFragmentContainer, fragment)
+                    .commit();
+        }
+    }
+}
